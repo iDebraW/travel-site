94ee2a10e89c74c71f5035e9ff9a239c9e57114a
Debra Wylie <iDebra@Debras-iMac.local>
2016-09-13 21:46:18 -0400

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer')
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log("Hooray - you created a Gulp task");
+});
+
+gulp.task('html', function(){
+  console.log("Imagine something useful being done here with your HTML.");
+});
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))//array [] to pass through pipe
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch',function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles')
+  });
+
+});
