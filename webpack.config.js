33e4b33c2cac73e97ff8836e54f8f2deec37d9cd
iDebraW <idebraw@gmail.com>
2016-11-08 15:32:37 -0500

    Completed lazy loading of images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: "./app/temp/scripts",
-    filename: "App.js"
+    filename: "[name].js"
   },
   module: {
     loaders: [
@@ -12,7 +15,7 @@ module.exports = {
           presets: ['es2015']
         },
         test: /\.js$/,
-        exclude: /node_modules/ 
+        exclude: /node_modules/
       }
     ]
   }
